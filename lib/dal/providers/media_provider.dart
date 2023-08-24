6a837f6a4e82e18e08dfbaa6b0557e6590f6d0ba
Meithal <hurin8888@gmail.com>
2023-08-24 02:14:46 +0200

    merge objet utilisé a un seul endroid

--- lib/dal/providers/media_provider.dart
+++ lib/dal/providers/media_provider.dart
@@ -1,8 +1,15 @@
 import 'package:backend_flutter_ivo/bo/media.dart';
 import 'package:backend_flutter_ivo/dal/generic_dao.dart';
-import 'package:backend_flutter_ivo/dal/media_access.dart';
 import 'package:backend_flutter_ivo/dal/providers/_i_provider.dart';
 import 'package:flutter/material.dart';
+
+final mediaAccess = Access<Media>(
+  collectionName: 'media',
+  instanceCreator: (s) => Media.fromMap(s),
+  // I didnt find how to have an instance created without reflection
+  // (dart:mirror) which seems to be unadvised to be used on flutter
+  // because of poor performance, so I embed the creator as a callback
+);
 
 class MediaProvider extends ChangeNotifier implements Iprovider<Media> {
   final Access<Media> mediaAccess_ = mediaAccess;
